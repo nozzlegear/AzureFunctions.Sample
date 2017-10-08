0ea28524fe06d0af8bdca99d39a90018bd2b613f
Joshua Harms <joshua@nozzlegear.com>
2017-10-07 22:19:56 -0500

    Add dotnet standard function

---
+++ Function3/Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace Function3
+{
+    public class Function
+    {
+        public static void Run(TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info("My C# timer is running!!!!!");
+        }
+    }
+}
